cb52fd54102eb979417ff0814b792e01ebe9e719
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-07-12 18:21:58 +0200

    Dragula lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'dragula-app';
+
+  vampires = [
+    { name: 'Dracula', favouriteColor: 'red' },
+    { name: 'Mrazomor', favouriteColor: 'white' },
+    { name: 'Krone', favouriteColor: 'green' },
+    { name: 'Tukidid', favouriteColor: 'blue' },
+  ];
+  zombies = [
+    { name: 'Lele', favouriteColor: 'red' },
+    { name: 'Majko', favouriteColor: 'white' },
+    { name: 'Ima', favouriteColor: 'green' },
+    { name: 'QRcode', favouriteColor: 'blue' },
+  ];
+
+  constructor() {}
 }
